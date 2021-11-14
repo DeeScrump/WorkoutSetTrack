9ae5d644e0c10b70ee9b91492524d46da6b19858
D Kornbluth <dwight.kornbluth@gmail.com>
2021-11-14 06:36:12 -0500

    added routes to index

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,18 @@
 const router = require("express").Router();
+const path = require("path");
 const Workout = require("../models/workoutModel.js");
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+app.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
 
 router.post("api/workouts")
 
